8917deff06a1f05e3453c613a1df646ac04387f7
adik-bao <akatre@uci.edu>
2024-07-17 12:21:46 -0700

    add movement
    FOR NIC: the movement scripts are in scripts directory.

---
+++ GSET Project/Assets/Scripts/PlayerStrafeAnimation.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStrafeAnimation : MonoBehaviour
+{
+    public float speed = 1000.0f;
+    [SerializeField] private Animator controller;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Ensure the Animator component is correctly referenced
+        if (controller == null)
+        {
+            Debug.LogError("Animator controller is not assigned.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) // Changed from GetKeyDown to GetKey for continuous checking
+        {
+            Debug.Log("Right arrow key is held down.");
+            controller.SetBool("PlayerMove", true);
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            Debug.Log("Right arrow key was released.");
+            transform.position = transform.position + new Vector3(0, 0, 0);
+            controller.SetBool("PlayerMove", false);
+        }
+    }
+}
